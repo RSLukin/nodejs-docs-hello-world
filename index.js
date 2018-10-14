8ab79d42c978f3fcfb5e8e72b6f470a1a79abd16
RSLukin <44001035+RSLukin@users.noreply.github.com>
2018-10-14 11:27:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! Test_NearTheFinal");
+    response.end("Hello World! Test_NearTheFinal_1.1");
 
 });
 
