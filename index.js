a371b04fa0cc029a36f174e5e5b0763a72b6df28
RSLukin <44001035+RSLukin@users.noreply.github.com>
2018-10-15 11:52:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! Test_NearTheFinal_1.1");
+    response.end("Hello World! Terraform_final_test");
 
 });
 
